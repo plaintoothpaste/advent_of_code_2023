39a670b34598682f05570ba95a042068ff1e9db7
Matthew McMillan <matthew.mcmillan@bigrep.com>
2023-12-06 19:09:55 +0100

    feature: day 4 now working with arbituary length of winners and selections

--- aoc_2023_4/cpp_p1/main.cpp
+++ aoc_2023_4/cpp_p1/main.cpp
@@ -1,53 +1,53 @@
 #include <algorithm>
-#include <array>
 #include <iostream>
 #include <numeric>
-#include <optional>
 #include <ranges>
 #include <string>
 #include <vector>
 
-#include "enumerate.h"
 #include "file_parse.h"
 
 int main() {
-    const std::function<std::vector<size_t>(std::string)> fn = [](const std::string& line) {
+    using pair_vec = std::vector<std::vector<size_t>>;
+    const std::function<pair_vec(std::string)> fn = [](const std::string& line) {
         // remove everything before the first colon
         // remove all letters and pipe |
         const auto start = line.find(':');
         const auto s = line.substr(start + 1);
-        const auto pipe = s.find('|');
-        const auto clean_str = s.substr(0, pipe) + s.substr(pipe + 1);
 
-        auto out = std::vector<size_t>{};
-        out.reserve(13);
+        auto out = pair_vec{};
+        out.resize(2);
         auto buffer = std::string{};
         buffer.reserve(10);
-
-        for (const char c : clean_str) {
-            if (c == ' ' && ! buffer.empty()) {
-                out.push_back(std::stoll(buffer));
+        auto getter = 0;
+        for (const char c : s) {
+            if ((c == ' ' || c == '|') && ! buffer.empty()) {
+                out[getter].push_back(std::stoll(buffer));
                 buffer.clear();
             }
-            if (c != ' ')
+            if (c == '|')
+                getter++;
+            else if (c != ' ')
                 buffer.push_back(c);
         }
         if (! buffer.empty()) {
-            out.push_back(std::stoll(buffer));
+            out[getter].push_back(std::stoll(buffer));
             buffer.clear();
         }
         return out;
     };
-    const auto data = parse("example.txt", fn);
+    const auto data = parse("input.txt", fn);
 
-    const auto fn_acc = [](const size_t& acc, std::vector<size_t> vec) {
-        // sort each section
-        std::sort(vec.begin(), vec.begin() + 5);
-        std::sort(vec.begin() + 5, vec.end());
+    const auto fn_acc = [](const size_t& acc, const std::vector<std::vector<size_t>>& vecs) {
+        auto winners = vecs[0];
+        auto selection = vecs[1];
+        // sort each
+        std::ranges::sort(winners);
+        std::ranges::sort(selection);
 
         auto coincident = std::vector<size_t>{};
         coincident.reserve(5);
-        std::set_intersection(vec.begin(), vec.begin() + 5, vec.begin() + 5, vec.end(), std::back_inserter(coincident));
+        std::ranges::set_intersection(winners, selection, std::back_inserter(coincident));
         if (coincident.empty())
             return acc;
         const auto points = static_cast<size_t>(pow(2, coincident.size() - size_t{ 1 }));
