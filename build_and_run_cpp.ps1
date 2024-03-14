f15bf095e8c40bce9e946d9737c7653d30d2d42f
Matthew McMillan <matthew.mcmillan@bigrep.com>
2024-03-14 10:51:21 +0100

    feature: ps1 works

--- build_and_run_cpp.ps1
+++ build_and_run_cpp.ps1
@@ -2,7 +2,7 @@ param (
     [Parameter(Mandatory=$true)][String]$day,
     [Parameter(Mandatory=$true)][String]$part
 )
-$common = './common/cpp'
+$common = "./common/cpp"
 $src = "./$day/cpp/$part"
 $origin = pwd
 
@@ -13,11 +13,11 @@ echo "Building: $src"
 cd $src
 cmake --preset release 
 cmake --build --preset release
-
-
-echo "Running: $src"
-echo "example"
-.\$day\cpp\$part\build\release\Release\aoc.exe .\$day\example.txt
-echo "main"
-.\$day\cpp\$part\build\release\Release\aoc.exe .\$day\input.txt
 cd $origin
+
+$exe = "$src/build/release/Release/aoc.exe"
+echo "Running: $exe"
+echo "example:"
+Start-Process -FilePath $exe -ArgumentList ['./$day/example.txt'] -Wait
+echo "main:"
+Start-Process -FilePath $exe -ArgumentList ['./$day/input.txt'] -Wait
